01be5837b24c0456ecd5a8b010a357bee1c435a8
Clinton Johnson <cmjsoftware@icloud.com>
2021-11-15 21:24:10 -0500

    Initial Setup. - Added HomeVM. - Added HomeView. - Added WhiteButton. - Added SmallVertialButton.

---
+++ Shared/BuildNetflexAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildNetflexAppApp.swift
+//  Shared
+//
+//  Created by Clinton Johnson on 11/12/21.
+//
+
+import SwiftUI
+
+@main
+struct BuildNetflexAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
